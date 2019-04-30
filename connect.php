e81caa743950db62e37a2a8005a99d6ddd700fdc
mzilley <45641662+mzilley@users.noreply.github.com>
2019-04-30 12:00:29 -0500

    Delete connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-
-<!--Connects to Database-->
-<?php
- $con = mysqli_connect("mysql4.uni.edu", "business", "HwHudY5JJYIZLPRy", "business_business");
- 	if (mysqli_connect_errno()){
-		die("Unsuccessful connection to database: " . mysqli_connect_error());
-	}
-?>
-
